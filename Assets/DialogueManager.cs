529800ca824b6cdfbccc1c087f575c578a2eaf01
Eva He <evahe@MacBook-Pro.local>
2022-04-24 21:25:26 +0100

    initial commit

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 /// <summary>
 /// the script is following this tutorial following some twitches https://youtu.be/_nRzoTzeyxU
 /// </summary>
-public class DialogueManager : MonoBehaviour {
+public class DialogueManager : MonoBehaviour
+{
 
 
 	public Text dialogueText;
@@ -18,51 +19,45 @@ public class DialogueManager : MonoBehaviour {
 	// Use this for initialization
 
 	// ive combined the dialogue trigger script with this one we probably dont need a trigger just need it to start working as soon as we entered the scene
-	void Start () {
+	void Start()
+	{
 		sentences = new Queue<string>();
+		StartDialogue(dialogue);
 	}
 
-	public void TriggerDialogue()
+	public void StartDialogue(Dialogue dialogue)
 	{
-		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-	}
-
-	public void StartDialogue (Dialogue dialogue)
-	{
-
-	
-		sentences.Clear();
-
+		Debug.Log("Started");
 		foreach (string sentence in dialogue.sentences)
 		{
 			sentences.Enqueue(sentence);
 		}
 
-		DisplayNextSentence();
+		
 	}
 
-	public void DisplayNextSentence ()
+
+
+	public void Update()
 	{
+		
+
 		if (sentences.Count == 0)
 		{
-			EndDialogue();
-			return;
-
-			//for this one probably needs to put dialogues in a loop
+			Debug.Log("Restarted");
+			foreach (string sentence in dialogue.sentences)
+			{
+				sentences.Enqueue(sentence);
+			}
 		}
 
 		//ok I'm making this up, need to say if mouse up anywhere or sth to trigger!!
-		if (Input.GetMouseButtonDown(0)) { 
+		if (Input.GetMouseButtonUp(0))
+		{
+			Debug.Log(sentences);
 			string sentence = sentences.Dequeue();
 			dialogueText.text = sentence;
 		}
 	}
-
-
-
-	void EndDialogue()
-	{
-		
-	}
-
 }
+
